292c8956d16f7eead93a53f1d0dd4d031af1efdc
SiEr0212 <simonertel@gmx.de>
2021-05-04 21:40:33 +0200

    added new component child

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import "./App.css";
 
 function App() {
+  const [i, setI] = useState(0);
+
+  const onClickHandle = () => {
+    setI(i + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+      <h3>Use Memo</h3>
+      <h2>i: {i}</h2>
+      <button onClick={onClickHandle}>Increment I</button>
+      </header>
     </div>
   );
 }
